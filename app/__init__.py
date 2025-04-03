e130b3aa153264abcdc378de72c119f85d1913c9
git <claytnmorais@gmail.com>
2025-04-03 00:33:16 -0300

    adicionando rota de health check

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 from app.model import init_db
 from app.route.patient_route import PatientRoute
+from app.route.health_check_route import HealthCheckRoute
 
 info = Info(title="Patient API", version="1.0.0")
 app = OpenAPI(__name__, info=info)
@@ -11,4 +12,5 @@ CORS(app)
 init_db()
 
 PatientRoute().init_routes(app)
+HealthCheckRoute().init_routes(app)
 
